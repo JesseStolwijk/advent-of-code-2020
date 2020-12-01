3c87320bb61e8e3509846d65317a0b0120df95bd
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-12-01 21:26:58 +0100

    Implement day 1

--- source/days/day1/part1.ts
+++ source/days/day1/part1.ts
@@ -6,21 +6,49 @@ export const runner = async () => {
 	const expenseReportLines = await parseFile(
 		path.resolve("./source/days/day1/input.txt")
 	);
-	const numbers = expenseReportLines.map(parseInt);
-	benchmark(() => part1(numbers), "day 1 part 1");
+	const numbers = expenseReportLines.map((n) => parseInt(n, 10));
+	// benchmark(() => findPairThatSumsTo(numbers), "day 1 part 1");
+
+	console.log(part2(numbers));
+
+	benchmark(() => part2(numbers), "day 1 part 2");
 };
 
-const SUM = 2020;
+const SUM_TO = 2020;
 
 // Ugly O(n) implementation
 const part1 = (numbers: number[]): number => {
+	const [a, b] = findPairThatSumsTo(numbers, SUM_TO)!;
+	return a * b;
+};
+
+const findPairThatSumsTo = (
+	numbers: number[],
+	sumTo: number
+): [number, number] | undefined => {
 	const set = new Set(numbers);
 
-	const luckyNumber = numbers.find((number) => set.has(SUM - number));
+	const foundNumber = numbers.find((number) => set.has(sumTo - number));
 
-	if (luckyNumber === undefined) {
-		throw new Error("Could not find the lucky number");
+	if (foundNumber === undefined) {
+		return undefined;
 	}
 
-	return luckyNumber * (SUM - luckyNumber);
+	return [foundNumber, sumTo - foundNumber];
+};
+
+// O(n^2) -_-
+const part2 = (numbers: number[]): number | undefined => {
+	for (let i = 0; i < numbers.length; i++) {
+		const currentNumber = numbers[i];
+		const maybeResult = findPairThatSumsTo(numbers, SUM_TO - currentNumber);
+
+		if (maybeResult) {
+			const [a, b] = maybeResult;
+
+			return a * b * currentNumber;
+		}
+	}
+
+	return undefined;
 };
--- source/file-parser.ts
+++ source/file-parser.ts
@@ -1,6 +1,6 @@
-import fs from 'fs';
+import fs from "fs";
 
-export const parseFile = async (path: string): Promise<string[]>  => {
-    const text = await fs.promises.readFile(path);
-    return text.toString('utf8').split("\n");
-}
+export const parseFile = async (path: string): Promise<string[]> => {
+	const text = await fs.promises.readFile(path);
+	return text.toString().split(/\r?\n/);
+};
